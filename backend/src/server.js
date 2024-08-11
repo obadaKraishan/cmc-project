18014e83c230a3d9eb8c32551d1351c746cdb43f
obada kraishan <obada.kraishan@gmail.com>
2024-08-11 09:23:05 -0500

    connect Mongo

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
